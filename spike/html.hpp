0d3d6ecad24c872d4f27f750834c2eafa36e62b9
Graham Eger <eger@umich.edu>
2019-01-27 13:58:29 -0500

    Think POSIXtive.

---
+++ spike/html.hpp
@@ -0,0 +1,17 @@
+//
+//  html.hpp
+//  engine
+//
+//  Created by Graham Eger on 1/26/19.
+//  Copyright © 2019 Graham Eger. All rights reserved.
+//
+
+#ifndef html_hpp_398
+#define html_hpp_398
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+
+#endif /* html_hpp */
