97c91ae8cf34b869a1737eeb0933c95c35a49977
Graham Eger <eger@umich.edu>
2019-02-05 14:32:16 -0500

    Trying out a new branch?

--- src/event.cpp
+++ src/event.cpp
@@ -8,15 +8,29 @@
 // 
 
 #include "event.hpp"
+#include <unistd.h>
 
 namespace search {
     EventQueue::EventQueue() {
         t = std::thread(&EventQueue::process, this);
+        #ifdef __linux__ 
+        epollFd = epoll_create1(0);
+        #else
+        kq = kqueue();
+        if (kq == -1) {
+            // TODO: log error
+        }
+        #endif
     }
 
     void EventQueue::process() {
+        std::vector<int> toAdd;
         while (true) {
-            
+            waitingSockets.pop_all(toAdd);
+            // we have a list of sockets that we want to wait on
+            #ifdef __linux__
+            #else
+            #endif
         }
     }
 
