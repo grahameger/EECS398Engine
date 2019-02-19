c2104ddc4e3fc8650838e1fe1d6c774a5dd5c477
Graham Eger <eger@umich.edu>
2019-02-19 18:52:17 -0500

    start work on the cralwer class

--- include/crawler.hpp
+++ include/crawler.hpp
@@ -10,16 +10,23 @@
 #ifndef crawler_hpp_398
 #define crawler_hpp_398
 
+#include <string>
+#include <vector>
+
 #include "semaphore.hpp"
+#include "http.hpp"
+#include "thread_queue.hpp"
 
 namespace search {
     class Crawler {
     public:
-        Crawler();
+        Crawler(const std::vector<std::string> &urls);
         ~Crawler();
     private:
         const static size_t MAX_CRAWLER_THREADS = 10000;
         threading::Semaphore<MAX_CRAWLER_THREADS> sem;
+        HTTPClient client;
+        threading::ThreadQueue<std::string> urls;
     };
 }
 
