a0197db67ee95bdfb57e2f98a8f8986f7067c79f
Jacob Crouch <jwcrouch@umich.edu>
2019-03-01 01:24:34 -0500

    All new. No more regex. Print out anchor text, title, body, links.

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -1,7 +1,6 @@
 //  Created by Jake C on 2/10/19.
-#ifndef Parser_h
-#define Parser_h
-#include <regex>
+#ifndef Parser_hpp
+#define Parser_hpp
 #include <iostream>
 #include <stdio.h>
 #include <sys/types.h>
@@ -9,7 +8,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-using std::regex;
 
 class LinkFinder {
 public:
@@ -19,47 +17,38 @@ public:
     //destructor
     ~LinkFinder();
     
-    //EFFECTS: Atomically rewrites filename with stripped html
-    //         Returns -1 on failed file creation, 0 on success
-    //MODIFIES: HTML file
-    int stripTags(const char* filename);
-    
-    //EFFECTS: Takes an HTML filename and creates a new file called links<filename>
-    //         Where each link is separated by a space. If no links found, file is empty.
-    //         Returns -1 on failed file creation, 0 on success
-    //MODIFIES: result[], Size
-    int findLinks(const char* filename);
+    //returns -1 if something failed, else returns 0
+    //parses html file into title, body, links, anchor
+    int parse(char *filename);
     
 private:
+    //sets index pointer to 1 place after string
+    //must know string can be found. will not work for malformed html
+    void find_string(char *html_file, char* find, long *index, long file_length);
+    //script tags
+    bool is_script(char *html_file, long *index, long file_length);
+    bool is_style(char *html_file, long *index, long file_length);
+    bool is_title(char *html_file, long *index, long file_length);
+    //prints word between > (here) < for title and anchor text
+    void get_anchor_word(char *html_file, long *index, long file_length, char* word);
+    
 };
-#endif /* Parser_h */
+#endif /* Parser_hpp */
 // -----------------------------------------------------------------------------
 //TESTING + HOW TO USE
 /*
  #include "Parser.h"
  #include <ctime>
- #include <fstream>
  using namespace std;
  
  int main(int argc, const char * argv[]) {
+ 
  std::clock_t start;
  double duration = 0;
  start = std::clock();
  LinkFinder L;
- L.findLinks(argv[1]);
  string word;
- int count = 0;
- ifstream ifs("linksfile3.txt");
- if(!ifs.is_open()) {
- cout << "Error opening linksfile3.txt" << endl;
- exit(EXIT_FAILURE);
- }
- while(ifs >> word) {
- cout << word << endl;
- count++;
- }
- cout << count << endl;
- L.stripTags(argv[1]); **ALWAYS CALL STRIPTAGS AFTER AS IT LITERALLY CHANGES FILE
+ L.parse((char*)argv[1]);
  duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
  std::cout<<"printf: "<< duration <<'\n';
  std::cout << "end";
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -6,7 +6,7 @@ LinkFinder::LinkFinder() {}
 //destructor
 LinkFinder::~LinkFinder() {}
 
-int LinkFinder::stripTags(const char* filename) {
+int LinkFinder::parse(char* filename) {
     int fd = open(filename, O_RDWR);
     if (fd < 0) {
         perror("file open");
@@ -18,7 +18,7 @@ int LinkFinder::stripTags(const char* filename) {
         return -1;
     };
     off_t file_size = mystat.st_size;
-    const char *html_file = (char*)mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    char *html_file = (char*)mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
     if (html_file == MAP_FAILED){
         printf("mmap error\n");
         return -1;
@@ -27,145 +27,208 @@ int LinkFinder::stripTags(const char* filename) {
         printf("failed to close file (errno=%d)",errno);
         return -1;
     }
-    std::regex reg("<(.*?)[\\s\\S]*?[\\s]*>");
-    // ATOMICITY:
-    // Choose a name for the temporary file.
-    // Write the new content to a temporary file.
-    // Flush the new content to disc.
-    // Move the temporary file onto the original.
-    //filename means pathname
-    char tmp_pathname[strlen(filename)+2];
-    snprintf(tmp_pathname,sizeof(tmp_pathname),"%s~",filename);
-    if (unlink(tmp_pathname)==-1) {
-        if (errno!=ENOENT) {
-            printf("failed to remove existing temporary file (errno=%d)",errno);
-            return -1;
+    long file_length = strlen(html_file);
+    long num = 0;
+    long *index = &num;
+    bool anchor_text_not_img = false;
+    while(*index < file_length) {//run until end of file
+        if(html_file[*index] == '<') {
+            (*index)++;
+            switch(html_file[*index]) {
+                case 'a'  : //Link
+                    if(html_file[*index+1] == ' ') {
+                        char find[] = "href=";
+                        find_string(html_file, find, index, file_length);
+                        std::cout << "link = ";
+                        while(html_file[*index] != ' ' && html_file[*index] != '>') {
+                            if(html_file[*index] != '"' && html_file[*index] != '\'') {
+                                std::cout << html_file[*index];
+                            }
+                            (*index)++;
+                        }
+                        std::cout << std::endl;
+                        
+                        if(html_file[*index - 1] != '>') {
+                            while(html_file[*index] != '>') {
+                                (*index)++;
+                            }
+                        }
+                        (*index)++;
+                        while(html_file[*index] != '<') {
+                            if(html_file[*index] != '\n' && html_file[*index] != ' ') {
+                                anchor_text_not_img = true;
+                            }
+                            if(anchor_text_not_img) {
+                                char anchor[] = "anchor";
+                                get_anchor_word(html_file, index, file_length, anchor);
+                                break;
+                            }
+                            (*index)++;
+                        }
+                        anchor_text_not_img = false;
+                        char find2[] = "</a>";
+                        find_string(html_file, find2, index, file_length);
+                    }
+                    //not a link, goto default
+                    else {
+                        goto DEFAULT;
+                    }
+                    break;
+                    
+                case 's'  : //script/style **Want to completely skip these
+                    if(is_style(html_file, index, file_length)) {
+                        char find[] = "</style>";
+                        find_string(html_file, find, index, file_length);
+                    }
+                    else if(is_script(html_file, index, file_length)) {
+                        char find[] = "</script>";
+                        find_string(html_file, find, index, file_length);
+                    }
+                    else {
+                        goto DEFAULT;
+                    }
+                    break;
+                    
+                case 't'  :
+                    if(is_title(html_file, index, file_length)) {
+                        char title[] = "title";
+                        get_anchor_word(html_file, index, file_length, title);
+                    }
+                    else {
+                        goto DEFAULT;
+                    }
+                    break;
+                    
+                DEFAULT:default : //not link or script or style tag, but still a tag just toss it
+                    char find[] = ">";
+                    find_string(html_file, find, index, file_length);
+            }
         }
-    }
-    mode_t default_mode = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH;
-    fd = open(tmp_pathname,O_RDWR|O_CREAT|O_TRUNC,default_mode);
-    if (fd==-1) {
-        printf("failed to open new file for writing (errno=%d)",errno);
-        return -1;
-    }
-    write(fd, std::regex_replace((char*)html_file, reg, "").c_str(), strlen(html_file) + 1);
-    if (fsync(fd)==-1) {
-        printf("failed to flush new file content to disc (errno=%d)",errno);
-        return -1;
-    }
-    if (close(fd)==-1) {
-        printf("failed to close new file (errno=%d)",errno);
-        return -1;
-    }
-    if (rename(tmp_pathname,filename)==-1) {
-        printf("failed to move new file to final location (errno=%d)",errno);
-        return -1;
+        else if(html_file[*index] == '\n' || html_file[*index] == ' ' || html_file[*index] == '\t') {
+            //do nothing
+            (*index)++;
+        }
+        else {//must be a word in text > word <
+            int count = 0;
+            long k = 0;
+            k = *index; //2 character anchor text minimum
+            while(k < file_length && count < 2 && html_file[k] != '<') {
+                if(html_file[k] != ' ' && html_file[k] != '\n' && html_file[k] != '\t') {
+                    count++;
+                }
+                k++;
+            }
+            if(count > 1) {
+                std::cout << "body = ";
+                while(html_file[*index] != '<') {
+                    std::cout << html_file[*index];
+                    (*index)++;
+                }
+                std::cout << std::endl;
+            }
+            else {
+                (*index)++;
+            }
+        }
     }
     return 0;
 }
 
-int LinkFinder::findLinks(const char* filename) {
-    int start_positions [2000];//assuming at most 2000 links per page
-    int end_positions [2000];
-    int start_index = 0;
-    int end_index = 0;
-    bool a_tag = false;
-    int fd = open(filename, O_RDWR);
-    if (fd < 0) {
-        perror("file open");
-        return -1;
-    };
-    struct stat mystat = {};
-    if (fstat(fd, &mystat)) {
-        perror("fstat");
-        return -1;
-    };
-    off_t file_size = mystat.st_size;
-    const char *html_file = (char*)mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (html_file == MAP_FAILED){
-        printf("mmap error\n");
-        return -1;
+void LinkFinder::get_anchor_word(char *html_file, long *index, long file_length, char* word) {
+    if(*index + 1 < file_length && html_file[*index + 1] == '<') {
+        //too small anchor text (only 1 char. worthless)
+        (*index)++;
+        return;
     }
-    //Iterate through file. Get start and end positions of links
-    int file_length = (int)strlen(html_file);
-    for(int i = 0; i < file_length; i++) {
-        //Found an <a or <A tag
-        if(i+2 < file_length && html_file[i] == '<' && (html_file[i+1] == 'a' || html_file[i+1] == 'A') && html_file[i+2] == ' ') {
-            start_positions[start_index] = i;
-            a_tag = true;
-            start_index++;
-            i += 2;
-        }
-        if(a_tag) {
-            //Found closing tag
-            if(html_file[i] == '>') {
-                end_positions[end_index] = i + 1;
-                end_index++;
-                a_tag = false;
-            }
-        }
+    std::cout << word << " = ";
+    while(*index < file_length && html_file[*index] != '<') {
+        std::cout << html_file[*index];
+        (*index)++;
     }
-    //use regex on each substring to find link
-    //**NOTE:Finds links if they exist i.e. href= isn't empty
-    regex regx("(?:href=[\"']([^ >\"'\n\t]+))|(?:href=([^ >\"'\n\t]+))", std::regex_constants::icase);
-    std::cmatch matches;
-    char tmp_pathname[strlen(filename)+2];
-    snprintf(tmp_pathname,sizeof(tmp_pathname),"%s~",filename);
-    if (unlink(tmp_pathname)==-1) {
-        if (errno!=ENOENT) {
-            printf("failed to remove existing temporary file (errno=%d)",errno);
-            return -1;
-        }
-    }
-    if (close(fd)==-1) {
-        printf("failed to close file (errno=%d)",errno);
-        return -1;
-    }
-    mode_t default_mode = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH;
-    fd = open(tmp_pathname,O_RDWR|O_CREAT|O_TRUNC,default_mode);
-    if (fd==-1) {
-        printf("failed to open new file for writing (errno=%d)",errno);
-        return -1;
-    }
-    for(int i = 0; i < end_index; i++) {
-        int length = end_positions[i] - start_positions[i] + 1;
-        char substring[length];
-        memcpy(substring, &html_file[start_positions[i]], end_positions[i] - start_positions[i]);
-        substring[end_positions[i] - start_positions[i]] = '\0';
-        if(std::regex_search(substring, matches, regx) ) {
-            if(!matches[1].matched) {
-                char add_space[strlen(matches[2].str().c_str()) + 2];
-                strcpy(add_space, matches[2].str().c_str());
-                strcat(add_space, " ");
-                if(write(fd, add_space, strlen(add_space)) != strlen(add_space)) {
-                    printf("Write didn't finish");
-                    return -1;
-                }
-            }
-            else {
-                char add_space[strlen(matches[1].str().c_str()) + 2];
-                strcpy(add_space, matches[1].str().c_str());
-                strcat(add_space, " ");
-                if(write(fd, add_space, strlen(add_space)) != strlen(add_space)) {
-                    printf("Write didn't finish");
-                    return -1;
-                }
-            }
-        }
-    }
-    if (fsync(fd)==-1) {
-        printf("failed to flush new file content to disc (errno=%d)", errno);
-        return -1;
-    }
-    if (close(fd)==-1) {
-        printf("failed to close new file (errno=%d)", errno);
-        return -1;
-    }
-    char prepend[strlen(filename) + 6];
-    strcpy(prepend, "links");
-    if (rename(tmp_pathname, strcat(prepend, filename)) == -1) {
-        printf("failed to move new file to final location (errno=%d)", errno);
-        return -1;
-    }
-    return 0;
+    std::cout << std::endl;
+    return;
 }
+
+
+bool LinkFinder::is_script(char *html_file, long *index, long file_length) {
+    if(*index + 5 < file_length) {
+        if(html_file[*index] == 's' && html_file[*index+1] == 'c' && html_file[*index+2] == 'r' && html_file[*index+3] == 'i' && html_file[*index+4] == 'p' && html_file[*index+5] == 't') {
+            *index += 5;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool LinkFinder::is_style(char *html_file, long *index, long file_length) {
+    if(*index + 4 < file_length) {
+        if(html_file[*index] == 's' && html_file[*index+1] == 't' && html_file[*index+2] == 'y' && html_file[*index+3] == 'l' && html_file[*index+4] == 'e') {
+            *index += 4;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool LinkFinder::is_title(char *html_file, long *index, long file_length) {
+    if(*index + 4 < file_length) {
+        if(html_file[*index] == 't' && html_file[*index+1] == 'i' && html_file[*index+2] == 't' && html_file[*index+3] == 'l' && html_file[*index+4] == 'e') {
+            *index += 6;
+            return true;
+        }
+    }
+    return false;
+}
+
+void LinkFinder::find_string(char *html_file, char* find, long *index, long file_length) {
+    int string_length = (int)strlen(find);
+    int counter = 0;
+    while(*index < file_length && counter < string_length) {
+        if(html_file[*index] == find[counter]) {
+            counter++;
+        }
+        else {
+            counter = 0;
+        }
+        (*index)++;
+    }
+    return;
+}
+
+
+//------------------------------------------------------------------------------
+// ATOMICITY:
+// Choose a name for the temporary file.
+// Write the new content to a temporary file.
+// Flush the new content to disc.
+// Move the temporary file onto the original.
+//filename means pathname
+/*  char tmp_pathname[strlen(filename)+2];
+ snprintf(tmp_pathname,sizeof(tmp_pathname),"%s~",filename);
+ if (unlink(tmp_pathname)==-1) {
+ if (errno!=ENOENT) {
+ printf("failed to remove existing temporary file (errno=%d)",errno);
+ return -1;
+ }
+ }
+ mode_t default_mode = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH;
+ fd = open(tmp_pathname,O_RDWR|O_CREAT|O_TRUNC,default_mode);
+ if (fd==-1) {
+ printf("failed to open new file for writing (errno=%d)",errno);
+ return -1;
+ }
+ char *p = " ";
+ write(fd, kko, length);
+ if (fsync(fd)==-1) {
+ printf("failed to flush new file content to disc (errno=%d)",errno);
+ return -1;
+ }
+ if (close(fd)==-1) {
+ printf("failed to close new file (errno=%d)",errno);
+ return -1;
+ }
+ if (rename(tmp_pathname,filename)==-1) {
+ printf("failed to move new file to final location (errno=%d)",errno);
+ return -1;
+ }
+ */
