9e4f833eaa409ab8289bc597598c7a8769476b1c
Jacob Crouch <jwcrouch@umich.edu>
2019-03-01 21:53:42 -0500

    Tested HTML parser. Parses title, links, anchor text, body. Open to more testing.

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -30,88 +30,91 @@ int LinkFinder::parse(char* filename) {
     long file_length = strlen(html_file);
     long num = 0;
     long *index = &num;
-    bool anchor_text_not_img = false;
     while(*index < file_length) {//run until end of file
         if(html_file[*index] == '<') {
             (*index)++;
             switch(html_file[*index]) {
                 case 'a'  : //Link
                     if(html_file[*index+1] == ' ') {
-                        char find[] = "href=";
-                        find_string(html_file, find, index, file_length);
-                        std::cout << "link = ";
-                        while(html_file[*index] != ' ' && html_file[*index] != '>') {
-                            if(html_file[*index] != '"' && html_file[*index] != '\'') {
-                                std::cout << html_file[*index];
-                            }
-                            (*index)++;
-                        }
-                        std::cout << std::endl;
-                        
-                        if(html_file[*index - 1] != '>') {
-                            while(html_file[*index] != '>') {
+                        char find_up[] = "HREF=";
+                        char find_low[] = "href=";
+                        long reset_value = *index;
+                        if(find_link(html_file, find_low, find_up, index, file_length)) {
+                            std::cout << "link = ";
+                            while(html_file[*index] != ' ' && html_file[*index] != '>') {
+                                if(html_file[*index] != '"' && html_file[*index] != '\'') {
+                                    std::cout << html_file[*index];
+                                }
                                 (*index)++;
                             }
+                            std::cout << std::endl;
                         }
-                        (*index)++;
-                        while(html_file[*index] != '<') {
-                            if(html_file[*index] != '\n' && html_file[*index] != ' ') {
-                                anchor_text_not_img = true;
-                            }
-                            if(anchor_text_not_img) {
-                                char anchor[] = "anchor";
-                                get_anchor_word(html_file, index, file_length, anchor);
-                                break;
-                            }
+                        
+                        //reset index in case link not found
+                        reset_index(index, reset_value);
+                        
+                        //close the a tag
+                        while(html_file[*index] != '>') {
                             (*index)++;
                         }
-                        anchor_text_not_img = false;
-                        char find2[] = "</a>";
-                        find_string(html_file, find2, index, file_length);
+                        //set ptr to before >
+                        (*index)--;
+                        //find a tag close. Parent or </a>
+                        find_closing_a_tag(html_file, index, file_length);
+                        while(html_file[*index] != '>') {
+                            (*index)++;
+                        }
+                        (*index)++;
                     }
-                    //not a link, goto default
+                    //not a link, was <a(some char), goto default since regular tag
                     else {
                         goto DEFAULT;
                     }
                     break;
                     
-                case 's'  : //script/style **Want to completely skip these
+                case 's'  : //script/style Want to completely skip these
                     if(is_style(html_file, index, file_length)) {
-                        char find[] = "</style>";
-                        find_string(html_file, find, index, file_length);
+                        char find_up[] = "</STYLE>";
+                        char find_low[] = "</style>";
+                        find_string(html_file, find_low, find_up, index, file_length);
                     }
                     else if(is_script(html_file, index, file_length)) {
-                        char find[] = "</script>";
-                        find_string(html_file, find, index, file_length);
+                        char find_up[] = "</SCRIPT>";
+                        char find_low[] = "</script>";
+                        find_string(html_file, find_low, find_up, index, file_length);
                     }
-                    else {
+                    else { //was not script or style tag, treat as ordinary tag
                         goto DEFAULT;
                     }
                     break;
                     
                 case 't'  :
+                    //found <t, if <title, get it
                     if(is_title(html_file, index, file_length)) {
                         char title[] = "title";
-                        get_anchor_word(html_file, index, file_length, title);
+                        get_words(html_file, index, file_length, title);
                     }
-                    else {
+                    else {//was not <title, treat as ordinary tag
                         goto DEFAULT;
                     }
                     break;
                     
-                DEFAULT:default : //not link or script or style tag, but still a tag just toss it
+                DEFAULT:default : //ordinary tag. Just skip everything between <...>
                     char find[] = ">";
-                    find_string(html_file, find, index, file_length);
+                    find_string(html_file, find, find, index, file_length);
             }
         }
+        //Inside >...< and not script or style, so body. Get it.
+        //ignore nothing
         else if(html_file[*index] == '\n' || html_file[*index] == ' ' || html_file[*index] == '\t') {
             //do nothing
             (*index)++;
         }
-        else {//must be a word in text > word <
+        //grab the body text
+        else {
             int count = 0;
             long k = 0;
-            k = *index; //2 character anchor text minimum
+            k = *index; //2 character text minimum
             while(k < file_length && count < 2 && html_file[k] != '<') {
                 if(html_file[k] != ' ' && html_file[k] != '\n' && html_file[k] != '\t') {
                     count++;
@@ -119,12 +122,10 @@ int LinkFinder::parse(char* filename) {
                 k++;
             }
             if(count > 1) {
-                std::cout << "body = ";
                 while(html_file[*index] != '<') {
-                    std::cout << html_file[*index];
-                    (*index)++;
+                    char body[] = "body";
+                    get_words(html_file, index, file_length, body);
                 }
-                std::cout << std::endl;
             }
             else {
                 (*index)++;
@@ -134,25 +135,13 @@ int LinkFinder::parse(char* filename) {
     return 0;
 }
 
-void LinkFinder::get_anchor_word(char *html_file, long *index, long file_length, char* word) {
-    if(*index + 1 < file_length && html_file[*index + 1] == '<') {
-        //too small anchor text (only 1 char. worthless)
-        (*index)++;
-        return;
-    }
-    std::cout << word << " = ";
-    while(*index < file_length && html_file[*index] != '<') {
-        std::cout << html_file[*index];
-        (*index)++;
-    }
-    std::cout << std::endl;
-    return;
-}
-
-
 bool LinkFinder::is_script(char *html_file, long *index, long file_length) {
     if(*index + 5 < file_length) {
         if(html_file[*index] == 's' && html_file[*index+1] == 'c' && html_file[*index+2] == 'r' && html_file[*index+3] == 'i' && html_file[*index+4] == 'p' && html_file[*index+5] == 't') {
+            *index += 5;
+            return true;
+        }
+        else if(html_file[*index] == 'S' && html_file[*index+1] == 'C' && html_file[*index+2] == 'R' && html_file[*index+3] == 'I' && html_file[*index+4] == 'P' && html_file[*index+5] == 'T') {
             *index += 5;
             return true;
         }
@@ -166,6 +155,10 @@ bool LinkFinder::is_style(char *html_file, long *index, long file_length) {
             *index += 4;
             return true;
         }
+        else if(html_file[*index] == 'S' && html_file[*index+1] == 'T' && html_file[*index+2] == 'Y' && html_file[*index+3] == 'L' && html_file[*index+4] == 'E') {
+            *index += 4;
+            return true;
+        }
     }
     return false;
 }
@@ -176,15 +169,19 @@ bool LinkFinder::is_title(char *html_file, long *index, long file_length) {
             *index += 6;
             return true;
         }
+        else if(html_file[*index] == 'T' && html_file[*index+1] == 'I' && html_file[*index+2] == 'T' && html_file[*index+3] == 'L' && html_file[*index+4] == 'E') {
+            *index += 6;
+            return true;
+        }
     }
     return false;
 }
 
-void LinkFinder::find_string(char *html_file, char* find, long *index, long file_length) {
-    int string_length = (int)strlen(find);
+void LinkFinder::find_string(char *html_file, char* find_lower, char* find_upper, long *index, long file_length) {
+    int string_length = (int)strlen(find_lower);
     int counter = 0;
     while(*index < file_length && counter < string_length) {
-        if(html_file[*index] == find[counter]) {
+        if(html_file[*index] == find_lower[counter]  || html_file[*index] == find_upper[counter]) {
             counter++;
         }
         else {
@@ -195,7 +192,204 @@ void LinkFinder::find_string(char *html_file, char* find, long *index, long file
     return;
 }
 
+bool LinkFinder::find_link(char *html_file, char* find_lower, char* find_upper, long *index, long file_length) {
+    int string_length = (int)strlen(find_lower);
+    int counter = 0;
+    while(*index < file_length && counter < string_length && html_file[*index] != '>') {
+        if(html_file[*index] == find_lower[counter]  || html_file[*index] == find_upper[counter]) {
+            counter++;
+        }
+        else {
+            counter = 0;
+        }
+        (*index)++;
+    }
+    if(counter == string_length) {
+        return true;
+    }
+    return false;
+}
 
+void LinkFinder::get_words(char *html_file, long *index, long file_length, char* word) {
+    while(*index < file_length && html_file[*index] != '<') {
+        if(html_file[*index] != '\n' && html_file[*index] != '\t' && html_file[*index] != ' ') {
+            std::cout << word << " = ";
+            while(html_file[*index] != '\n' && html_file[*index] != '\t' && html_file[*index] != ' ' && html_file[*index] != '<') {
+                std::cout << html_file[*index];
+                (*index)++;
+            }
+            std::cout << std::endl;
+        }
+        else {
+            (*index)++;
+        }
+    }
+}
+
+void LinkFinder::get_anchor_text(char *html_file, long *index, long file_length, long stop_index) {
+    char anchor[] = "anchor";
+    //Skip over all inner tags until we hit a's closing tag
+    while(*index < stop_index) {
+        if(html_file[*index] == '<') {
+            while(html_file[*index] != '>') {
+                (*index)++;
+            }
+            (*index)++;
+        }
+        get_words(html_file, index, file_length, anchor);
+    }
+    *index = stop_index;
+    return;
+}
+
+void LinkFinder::reset_index(long *index, long reset_value) {
+    *index = reset_value;
+    return;
+}
+
+long LinkFinder::get_min(long index1, long index2) {
+    return (index1 < index2) ? index1 : index2;
+}
+
+long LinkFinder::get_max(long index1, long index2) {
+    return (index1 > index2) ? index1 : index2;
+}
+
+void LinkFinder::find_closing_a_tag(char *html_file, long *index, long file_length) {
+    long reset_value = *index + 2;
+    long parent_end_position = 0;
+    
+    //find first open tag
+    if(find_open_tag(html_file, index, file_length)) {
+        //found parent opening <
+        //get length, then name
+        int length = 2;
+        while(*index < file_length && html_file[*index] != '>') {
+            length++;
+            (*index)++;
+        }
+        reset_index(index, *index - length + 2);
+        char parent_tag[length];
+        int i = 0;
+        for(; i < length - 1; i++) {
+            parent_tag[i] = html_file[*index];
+            (*index)++;
+        }
+        parent_tag[i] = '\0';
+        //find parent closing tag position
+        parent_end_position = parent_tag_distance(html_file, parent_tag, index, file_length);
+    }
+    else {
+        parent_end_position = 0;//ensures </a> is used
+    }
+    
+    //reset file pointer
+    reset_index(index, reset_value);
+    
+    //find closing a tag position
+    char find_lowa[] = "</a>";
+    char find_upA[] = "</A>";
+    find_string(html_file, find_lowa, find_upA, index, file_length);
+    
+    //actual tag close will be min distance of parent and a tag
+    long stop_index = get_min(parent_end_position, *index - 4);
+    
+    //in case of no parent, don't infinite loop
+    if (stop_index <= reset_value) {
+        stop_index = get_max(parent_end_position, *index - 4);
+    }
+    
+    //reset file pointer
+    reset_index(index, reset_value);
+    
+    //Get the anchor text
+    get_anchor_text(html_file, index, file_length, stop_index);
+    return;
+}
+
+
+long LinkFinder::parent_tag_distance(char *html_file, char* tag, long *index, long file_length) {
+    int string_length = (int)strlen(tag);
+    char end_tag[string_length + 2];
+    end_tag[0] = '<';
+    end_tag[1] = '/';
+    int j = 2;
+    for(int i = 1; i < string_length; i++) {
+        end_tag[j] = tag[i];
+        j++;
+    }
+    end_tag[j] = '\0'; //now = </parent>
+    bool nested_tag = false;
+    int counter = 0; //don't think theres a tag that will mess this up <gggggg>
+    int end_tag_counter = 0;
+    
+    //Go through file and try to find parent's closing tag.
+    //Don't get tricked by nested tags
+    while(*index < file_length) {
+        if(html_file[*index] == end_tag[end_tag_counter]) {
+            end_tag_counter++;
+            if(end_tag_counter == string_length + 1) {
+                if(!nested_tag) {
+                    (*index) -= (strlen(end_tag) - 1);
+                    return *index;
+                }
+                else {
+                    end_tag_counter = 0;
+                    nested_tag = false;
+                }
+            }
+        }
+        else {
+            end_tag_counter = 0;
+        }
+        if(html_file[*index] == tag[counter]) {
+            counter++;
+            if(counter == string_length) {
+                nested_tag = true;
+                counter = 0;
+            }
+        }
+        else {
+            counter = 0;
+        }
+        (*index)++;
+    }
+    return *index;
+}
+
+bool LinkFinder::find_open_tag(char *html_file, long *index, long file_length) {
+    bool is_closing_tag = false;
+    bool tag_found = false;
+    int num_closing = 0;
+    int num_opening = 0;
+    while(*index > 0) {
+        while(*index > 0 && html_file[*index] != '>') { //find a tag
+            if(html_file[*index] == '>') {
+                tag_found = true;
+            }
+            (*index)--;
+        }
+        if(!tag_found) { //no parent tag found
+            return false;
+        }
+        while(*index > 0 && html_file[*index] != '<') { //tag was found
+            if(html_file[*index] == '/') {
+                num_closing++; //uneven tag
+                is_closing_tag = true; //is closing tag
+            }
+            (*index)--;
+        }
+        if(!is_closing_tag) {
+            num_opening++; //Got here, then must be opening tag
+        }
+        is_closing_tag = false; //reset
+        if(num_opening >= num_closing) { //check if tags are even
+            return true;
+        }
+        (*index)++;
+    }
+    return false;//never get here
+}
 //------------------------------------------------------------------------------
 // ATOMICITY:
 // Choose a name for the temporary file.
