c4cccc39a36de2c565b8bba3d380386df0df02f6
Jacob Crouch <jwcrouch@umich.edu>
2019-03-01 21:53:42 -0500

    Tested HTML parser. Parses title, links, anchor text, body. Open to more testing.

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -18,31 +18,61 @@ public:
     ~LinkFinder();
     
     //returns -1 if something failed, else returns 0
-    //parses html file into title, body, links, anchor
+    //parses html file into title, body, links, and anchor text
     int parse(char *filename);
     
 private:
     //sets index pointer to 1 place after string
-    //must know string can be found. will not work for malformed html
-    void find_string(char *html_file, char* find, long *index, long file_length);
-    //script tags
+    //must know string can be found.
+    //will skip entire html file if no </a> on the page. else skips to next </a>
+    //be careful to reset index pointer to ensure still in range
+    void find_string(char *html_file, char* find_lower, char* find_upper, long *index, long file_length);
+    
+    //Look for href= If doesn't exist in <a>, returns false. Else, true.
+    bool find_link(char *html_file, char* find_lower, char* find_upper, long *index, long file_length);
+    
+    //If tag found, returns true. Else, false;
     bool is_script(char *html_file, long *index, long file_length);
     bool is_style(char *html_file, long *index, long file_length);
     bool is_title(char *html_file, long *index, long file_length);
-    //prints word between > (here) < for title and anchor text
-    void get_anchor_word(char *html_file, long *index, long file_length, char* word);
     
+    //Prints out words line by line and which tag(word) they belong to
+    void get_words(char *html_file, long *index, long file_length, char* word);
+    
+    //resets the file pointer to reset_value. Good to use after find_string.
+    void reset_index(long *index, long reset_value);
+    
+    //Prints out all words from *index to stop_index. Ignores everything in <...>
+    void get_anchor_text(char *html_file, long *index, long file_length, long stop_index);
+    
+    //Finds <a> tag's parent tag if exists. Then finds position of either the
+    //parent's closing tag or </a>, whichever occurs first. Then prints out anchor
+    //text between <a>....position.
+    void find_closing_a_tag(char *html_file, long *index, long file_length);
+    
+    //returns min of index1 and index2
+    long get_min(long index1, long index2);
+    
+    //returns max of index1 and index2
+    long get_max(long index1, long index2);
+    
+    //sets file pointer to start of opening parent tag
+    bool find_open_tag(char *html_file, long *index, long file_length);
+    
+    //If parent tag exists, returns position of parent tag close.
+    long parent_tag_distance(char *html_file, char* tag, long *index, long file_length);
 };
 #endif /* Parser_hpp */
 // -----------------------------------------------------------------------------
 //TESTING + HOW TO USE
 /*
- #include "Parser.h"
+ #include "Parser.hpp"
  #include <ctime>
  using namespace std;
  
- int main(int argc, const char * argv[]) {
+ **takes a filename as an argument**
  
+ int main(int argc, const char * argv[]) {
  std::clock_t start;
  double duration = 0;
  start = std::clock();
