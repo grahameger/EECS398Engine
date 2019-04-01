b288bb3f7e274020a100a951c86af49253229810
Jacob Crouch <jwcrouch@umich.edu>
2019-04-01 19:14:46 -0400

    Initial query compiler commit

---
+++ include/expression.h
@@ -0,0 +1,94 @@
+/*
+ * expression.h
+ *
+ * Class declarations for expressions
+ *
+ */
+
+#ifndef EXPRESSION_H_
+#define EXPRESSION_H_
+
+#include <stdint.h>
+#include <vector>
+#include <cstddef>
+#include <string>
+using namespace std;
+
+/**
+ * Just a plain old expression
+ */
+class Expression
+   {
+public:
+
+   virtual ~Expression( );
+
+   //virtual int64_t Eval( ) const = 0;
+       virtual string stringEval( ) const = 0;
+   };
+// class Expression
+class Phrase: public Expression
+{
+protected:
+    
+    string value;
+    
+public:
+    
+    Phrase( string val );
+    
+    string stringEval( ) const override;
+};
+
+class ANDExpression : public Expression
+{
+protected:
+    std::vector < Expression * > terms;
+public:
+    void addTerm(Expression *);
+    
+    string stringEval( ) const override
+    {
+        string phrase = terms[ 0 ]->stringEval( );
+        for ( size_t i = 1;  i < terms.size( );  ++i ) {
+            phrase += "&" + terms[ i ]->stringEval( );
+        }
+        return phrase;
+    }
+};
+
+class AddExpression : public Expression
+{
+protected:
+    std::vector < Expression * > terms;
+public:
+    void addTerm(Expression *);
+    
+    string stringEval( ) const override
+    {
+        string phrase = terms[ 0 ]->stringEval( );
+        for ( size_t i = 1;  i < terms.size( );  ++i ) {
+            phrase += " " + terms[ i ]->stringEval( );
+        }
+        return phrase;
+    }
+};
+
+class OrExpression : public Expression
+{
+protected:
+    std::vector < Expression * > terms;
+public:
+    void addTerm(Expression *);
+    
+    string stringEval( ) const override
+    {
+        string phrase = terms[ 0 ]->stringEval( );
+        for ( size_t i = 1;  i < terms.size( );  ++i ) {
+            phrase += "|" + terms[ i ]->stringEval( );
+        }
+        return phrase;
+    }
+};
+
+#endif /* EXPRESSION_H_ */
---
+++ include/query_tokenstream.h
@@ -0,0 +1,87 @@
+/*
+ * query_tokenstream.h
+ *
+ * Declaration of a stream of tokens that you can read from
+ *
+ */
+
+#include pragma once
+#ifndef QUERY_TOKENSTREAM_H_
+#define QUERY_TOKENSTREAM_H_
+
+#include <string>
+#include "expression.h"
+
+
+/**
+ * Check if a character is relevant to a math expression
+ *
+ * Relevant characters are defined as
+ * +
+ * -
+ * *
+ * (
+ * )
+ *
+ * and all digits
+ */
+bool CharIsRelevant( char c );
+
+/**
+ * Opposite of char is relevant. Needed for filtering the input
+ */
+bool CharIsIrrelevant( char c );
+
+/**
+ * The token stream, which you can both Match( ) a single character from,
+ * or ParseWord( ) to consume a whole word
+ *
+ * The input string by default is filtered of any characters that are
+ * deemed "irrelevant" by the CharIsIrrelevant function above
+ */
+
+bool is_char(const char c);
+
+void help_message();
+
+
+class TokenStream
+   {
+   // The input we receive, with only relevant characters left
+   std::string input;
+   // Where we currently are in the input
+   size_t location { 0 };
+
+public:
+
+   /**
+    * Construct a token stream that uses a copy of the input
+    * that contains only characters relevant to math expressions
+    */
+   TokenStream( const std::string &in );
+
+   /**
+    * Attempt to match and consume a specific character
+    *
+    * Returns true if the char was matched and consumed, false otherwise
+    */
+   bool Match( char c );
+       
+
+   /**
+    * Check whether all the input was consumed
+    */
+   bool AllConsumed( ) const;
+       
+       
+    /**
+    * Attempt to match and consume a whole word
+    *
+    * Return a dynamically allocated phrase if successful, nullptr otherwise
+    */
+       
+    Phrase *parseWord( );
+
+   };
+
+#endif /* TOKENSTREAM_H_ */
