0f9d63c8fc6fcda8ccd0c719ef473ad40cbedd90
Max Guthmann <maxguthmann11@gmail.com>
2019-04-08 17:54:15 -0400

    helpers and docEnd

--- include/index.h
+++ include/index.h
@@ -1,6 +1,6 @@
 #include "String.h"
 #include "vector.h"
-
+#include "threading.h"
 
 
 
@@ -18,22 +18,77 @@ private:
 	int findWordBlock(String word);
 	int hash(String word);
 	int hash2(String word);
-	void incrementNextEmptyBlock();
+	int incrementNextEmptyBlock();
 
+   //reading and writing functions
+   void readBlock(char* buf, int blockNum);
+   void readLocaiton(char* buf, int blockNum, int offset, int length);
+   void writeBlock(char* buf, int blockNum);
+   void writeLocation(char* buf, int blockNum, int offset, int length);
+   //input is char* of a block with int at the end which is a pointer, output is the same char* containing block with pointer = 0
+   void followPointer(char* buf, int blockNum);
 	//VARIABLES
 	vector<int> currentBlocks;
+	const int blockSize = 8192;
+	
 	int fd;
-	int blockSize = 8192;
 	int numOfPostingSizes = ;
 	vector<int> postingBlockSizes = {  };
-	int pageEndBlock = 1025;
-	int urlBlock = 1026;
+	const int pageEndBlock = 1025;
+	const int urlBlock = 1026;
 	long currentLocation = 0;//increment at end of addWord and newDoc
 	//increment next empty block, if out of blocks make the file bigger
 	int nextEmptyBlock = 1028;
 	int numBlocks = 10000;
 	int currentDocId = 0;
-	struct urlMetadata{
+   
+   //THREADING
+   vector<ReadWriteLock> locks;
+	mutex nextBlockLock;
+   struct urlMetadata{
 		//not sure what we need here
 	};
+};
+
+class Post{
+
+}
+
+class PostingList{
+
+}
+
+class WordIndex{
+   //this is the index that maps posting lists to block offsets
+public:
+   WordIndex(char* buf, int startOffset, int endOffset);
+   int findWord(String word);
+   void update(String word, int offset);
+private:
+   String index; 
+
+
+}
+
+class PostingListIndex{
+   //this is the index contained in a posting list that contains offsets to posts
+public:
+   //for reading in a posting list index
+   PostingListIndex(char* buf, int startOffset, int endOffset);
+   //for creating a new posting list index
+   PostingListIndex(String location, int length);
+   
+   //returns the largest location in this index
+   int largestLocation();
+   //returns the first unused charecter in this posting list
+   int nextOpenChar();
+   int size();
+   //updates the index with new post at offset in block, location in internet, size of post
+   void update(String location, String offset, int length);
+   String string();
+   //returns location of index pointer URL and page end blocks
+   int pointer();
+private:
+   String index; 
+   const int blockSize = 8192
 };
--- src/index.cpp
+++ src/index.cpp
@@ -3,7 +3,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-	Index::Index(String filename){
+	Index::Index(String filename):locks(1028) {
 		fd = open(filename.CString(), O_RDWR | O_CREAT);
 		if(fd == -1){
 
@@ -18,6 +18,9 @@
 		}
 		currentBlocks.resize(numOfPostingSizes, 0);
 		currentBlocks[0] = 1027;
+      for(int i = 0; i<1028; i++){
+         locks[i] = ReadWriteLock();
+      }
     }
 
 	void Index:newDoc(string url){
@@ -26,7 +29,6 @@
 		//put page metadata in url list
 
 		//design of page end block:
-			//number of page end blocks, starts with 0, if greater than 0 dereference int at end of block X times to get to block with available space
 			//offset to index
 			//posts
 			//empty space
@@ -46,161 +48,116 @@
 		//step 4 current locaiton++
 		//step 5 find location to place url+metadata
 		//step 6 update url block index
-		if(lseek(fd, pageEndBlock*blockSize, SEEK_SET) ==-1){
+      locks[pageEndBlock].writeLock();
+      char* buf = new char[blockSize];
+      readBlock(buf, pageEndBlock);
+		//get newest url block
+      int blockNum = followPointer(buf);
 
-		}
-		char* buf = new char[blockSize];
-		if(read(fd, buf, blockSize) == -1){
-
-		}
-		int numPageEndBlocks;
-		memcpy(&numPageEndBlocks, buf, sizeof(int));
 		int indexPointer;
-		memcpy(&indexPointer, buf + 4, sizeof(int));
-		int pointer = 0;
-		while(numPageEndBlocks > 0){
-			memcpy(&pointer, buf + 8187, sizeof(int));
-			if(lseek(fd, pointer*blockSize, SEEK_SET) ==-1){
+		memcpy(&indexPointer, buf, sizeof(int));
 
-			}
-			if(read(fd, buf, blockSize) == -1){
 
-			}
-			numPageEndBlocks--;
-			if(numPageEndBlocks == 0){
-				//index pointer lives at char0 in all page end blocks othe than the first
-				memcpy(&indexPointer, buf, sizeof(int));
-			}
-		}
+      //need the location in utf8 form for the index
 		String utf8Location = utf8(currentLocation);
 		//read in the index
 		PostingListIndex index = PostingListIndex(buf, blockSize-1-sizeof(int) ,indexPointer);
 		//find offset and real location of most recently added page
-		index.findNewestPost();
+		//index.findNewestPost();
 		int delta = currentLocation - index.largestLocation()
 		//convert delta to utf8 for saving
 		String utf8Delta = utf8(delta);
+      //add new delta this must be done before index.update
+      memcpy(buf + index.nextOpenChar(), utf8Delta.CString(), utf8Delta.Size());
 		//check if there is space to add page end
-		if(utf8Delta.Size() + index.nextOpenChar() > indexPointer){
+		index.update(utf8Location, index.nextOpenChar(), utf8Delta.Size());
+		if(utf8Delta.Size() + index.nextOpenChar() > index.pointer()){
 			//not enough space, need to move to next block
 			char* pointerWriter[sizeof(int)];
+			nextBlockLock.lock();
+         int nextBlock = incrementNextEmptyBlock();
+			nextBlockLock.unlock();
 			//need to convert nextEmptyBlock to a cstring to write it
-			memcpy(pointerWriter, &nextEmptyBlock, sizeof(int));
-			if(lseek(fd, 8187, SEEK_CUR) ==-1){
+         memcpy(pointerWriter, &nextBlock, sizeof(int));
+         //update block pointer, buf was fucked up by memcpying the delta when it didnt fit, so I use pointer writer
+         writeLocation(pointerWriter, blockNum, blockSize - 1 - sizeof(int), sizeof(int));		
+         //locks[blockNum] is write locked by followPointer()
+         locks[blockNum].unlock();
 
-			}
-			//write pointer to new block in current block
-			if(write(fd, pointerWriter, sizeof(int)) == -1){
-
-			}
-			if(lseek(fd, nextEmptyBlock*blockSize, SEEK_SET) ==-1){
-
-			}
-			if(read(fd, buf, blockSize) == -1){
-
-			}
-			incrementNextEmptyBlock();
-			//TODO set index pointer, create index
-			if(lseek(fd, 4, SEEK_CUR) ==-1){
-
-			}
-			if(write(fd, utf8Location.CString(), utf8Location.size());
+         //create a new url block
+         locks[nextBlock].writeLock();
+         readBlock(buf, nextBlock);
+         PostingListIndex newIndex = PostingListIndex(utf8Location, utf8Location.Size());
+         //last byte in block is blockSize-1, first byte of blockPointer is that - sizeof(int), first block of index is that - index.size()
+         indexPointer = newIndex.pointer();
+         //update indexPointer
+         memcpy(buf, &indexPointer, sizeof(int));
+         //put in first delta 
+         memcpy(buf + sizeof(int), utf8Delta.CString(), utf8Delta.Size());
+         //put in index
+         memcpy(buf + indexPointer, newIndex.string().CString(), newIndex.size());
+         //save it
+         writeBlock(buf, nextBlock);
+         locks[nextBlock].unlock();
 		}
 		else{
-			char utf8DeltaChar = new char[utf8Delta.Size()];
-			strcpy(buf + index.nextOpenChar(), utf8DeltaChar);
-			//TODO update index
+         //update index pointer
+         indexPointer = index.pointer();
+         memcpy(buf, &indexPointer, sizeof(int));
+         //update index
+         memcpy(buf + indexPointer, Index.string().CString(), index.size());
 			
 			//save the block
-			if(pointer != 0){
-				if(lseek(fd, pointer*blockSize, SEEK_SET) ==-1){
-
-				}
-			}
-			else{
-				if(lseek(fd, pageEndBlock*blockSize, SEEK_SET) ==-1){
-
-				}
-				
-			}
-			if(write(fd, buf, blockSize) == -1){
-
-			}
-
+         writeBlock(buf, blockNum);
+         locks[blockNum].unlock();
 		}
 		currentLocation++;
 		//this concludes the page end portion of newDoc()
-		if(lseek(fd, blockSize*urlBlock, SEEK_SET) ==-1){
-		
-		}
-		if(read(fd, buf, blockSize) == -1){
+		locks[urlBlock].writeLock();
+      readBlock(buf, urlBlock);
+      //get newest url block
+		int blockNum = followPointer(buf);
 
-		}
-		memcpy(&pointer, buf + 8187, sizeof(int));
-		int blockNum = 0;
-		while(pointer != 0){
-			//follow the pointer until it is 0 signifiying a non full block
-			blockNum = pointer;//so that the block num doesn't get written over a the end of the loop
-			if(lseek(fd, blockSize*pointer, SEEK_SET) ==-1){
-		
-			}
-			if(read(fd, buf, blockSize) == -1){
-
-			}
-			memcpy(&pointer, buf + 8187, sizeof(int));
-			
-		}	
 		memcpy(&indexPointer, buf, sizeof(int)); 
 		PostingListIndex urlIndex = PostingListIndex(buf, blockSize-1-sizeof(int) ,indexPointer);
-		index.findNewestPost();		
 		//is url passed as utf8?
 		String utf8Url = urlAndDataToUtf8(url);
-		if(utf8Url.Size() + index.nextOpenChar() > indexPointer){
+      //add new url
+      memcpy(buf + urlIndex.nextOpenChar(), utf8Url.CString(), utf8Url.Size());
+		//url fits in this block
+		urlIndex.update(currentDocId, urlIndex.nextOpenChar(), utf8Url.Size());
+		if(utf8Url.Size() + index.nextOpenChar() > urlIndex.pointer()){
 			//not enough space in current block, must make a new one
-			memcpy(pointerWriter, &nextEmptyBlock, sizeof(int));
-			if(lseek(fd, 8187, SEEK_CUR) ==-1){
-
-			}
+			char* pointerWriter[sizeof(int)];
+			nextBlockLock.lock();
+         int nextBlock = incrementNextEmptyBlock();
+			nextBlockLock.unlock();
+			memcpy(pointerWriter, &nextBlock, sizeof(int));
 			//write pointer to new block in current block
-			if(write(fd, pointerWriter, sizeof(int)) == -1){
+         writeLocation(pointerWriter, blockNum, blockSize - 1 - sizeof(int), sizeof(int));		
+			locks[blockNum].unlock();
 
-			}
-			if(lseek(fd, nextEmptyBlock*blockSize, SEEK_SET) ==-1){
 
-			}
-			if(read(fd, buf, blockSize) == -1){
-
-			}
-			incrementNextEmptyBlock();
-			//TODO update index
-			if(lseek(fd, 4, SEEK_CUR) ==-1){
-
-			}
-			if(write(fd, utf8Location.CString(), utf8Location.size() == -1){
-
-			}
+         locks[nextBlock].writeLock();
+         readBlock(buf, nextBlock);
+         PostingListIndex newIndex = PostingListIndex(currentDocId, utf8Url.Size());
+         //update index pointer
+         memcpy(buf, newIndex.pointer(), sizeof(int));
+         //add new url
+         memcpy(buf + sizeof(int), utf8Url.CString(), utf8Url.Size());
+      	//update index
+      	memcpy(buf + newIndex.pointer(), newIndex.string().CString(), newIndex.Size());
+         writeBlock(buf, nextBlock);
+		   locks[nextBlock].unlock();
 		}
 		else{
-			//url fits in this block
-			if(blockNum == 0){
-				if(lseek(fd, urlBlock*blockSize + index.nextOpenChar();, SEEK_SET) ==-1){
-
-				}
-				if(write(fd, utf8Location.CString(), utf8Location.size() == -1){
-
-				}
-			}
-			else{
-				if(lseek(fd, blockNum*blockSize + index.nextOpenChar();, SEEK_SET) ==-1){
-
-				}
-				if(write(fd, utf8Location.CString(), utf8Location.size() == -1){
-
-				}
-
-			}
-			//TODO update index
-		}
+         //update index pointer
+         memcpy(buf, urlIndex.pointer(), sizeof(int));
+      	//update index
+      	memcpy(buf + urlIndex.pointer(), urlIndex.string().CString(), urlIndex.Size());
+         writeBlock(buf, blockNum);
+		   locks[blockNum].unlock();
+      }
 		currentDocId++;
 		delete[] buf;		
 	}
@@ -212,19 +169,78 @@ void Index::addWord(String word, String wordData){
 
 }
 
+void Index::followPointer(char* buf, int blockNum){
+   int pointer = 0;
+   memcpy(&pointer, buf + blockSize - 5, sizeof(int));
+   while( pointer != 0){
+      locks[blockNum].unlock();
+      blockNum = pointer;
+      locks[blockNum].writeLock();
+      readBlock(buf, blockNum);
+      memcpy(&pointer, buf + blockSize - 5, sizeof(int));
+   }
+   return blockNum;
+}
 
 void Index::findWordBlock(String word){
 
 }
 
 
-void incrementNextEmptyBlock(){
-	if(nextEmptyBlock == numBlocks - 1){
+void Index::incrementNextEmptyBlock(){
+   if(nextEmptyBlock == numBlocks - 1){
 		numBlocks *= 2;
 		if(ftruncate(fd, numBlocks*blockSize) == -1){
 
 		}
 		
 	}
-	nextEmptyBlock++;
+   ReadWriteLock blockLock;
+   locks.push_back(blockLock);
+	//return nextEmptyBlock then increment
+   return nextEmptyBlock++;
 }
+
+void Index::readBlock(char* buf, int blockNum){
+   if(lseek(fd, blockNum*blockSize, SEEK_SET) == -1){
+
+   }
+   if(read(fd, buf, blockSize){
+
+   }
+}
+
+void Index::writeBlock(char* buf, int blockNum){
+   if(lseek(fd, blockNum*blockSize, SEEK_SET) == -1){
+
+   }
+   if(write(fd, buf, blockSize){
+
+   }
+}
+
+void Index::readLocation(char* buf, int blockNum, int offset, int length){
+   if(lseek(fd, blockNum*blockSize + offset, SEEK_SET) == -1){
+
+   }
+   if(read(fd, buf, length){
+
+   }
+}
+
+void Index::writeLocation(char* buf, int blockNum, int offset, int length){
+   if(lseek(fd, blockNum*blockSize + offset, SEEK_SET) == -1){
+
+   }
+   if(write(fd, buf, length){
+
+   }
+}
+
+int PostingListIndex::pointer(){
+   return blockSize - 1  - sizeof(int) - size;
+
+}
+
+
+
