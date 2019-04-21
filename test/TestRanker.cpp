381cb8670b03c25e29fd82d3c84afd3549b0aa14
dennli <dennli@umich.edu>
2019-04-20 20:13:25 -0400

    runs but has bugs

--- test/TestRanker.cpp
+++ test/TestRanker.cpp
@@ -1,6 +1,7 @@
 #include "vector.h"
 #include "Ranker.h"
 #include <vector>
+#include <stdio.h>
 
 void LoadVector(Vector<Location>& dst, std::vector<Location>& src)
    {
@@ -17,24 +18,56 @@ int main()
    std::vector<Location> dennisBodySrc = {3, 13, 24};
    std::vector<Location> dennisTitleSrc = {21};
    std::vector<Location> dennisUrlSrc = {7, 14, 16, 23};
+   //todo make it so that it will always take me to diff document
+   std::vector<Location> rootIsrSrc = {2, 12, 21};
    //std::vector<Location> liSrc = {14, 15, 18};
 
    Vector<Location> docEndLocations;
    Vector<Location> dennisAnchorLocations;
+   Vector<Location> dennisBodyLocations;
+   Vector<Location> dennisTitleLocations;
+   Vector<Location> dennisUrlLocations;
+   Vector<Location> rootIsrLocations;
    //Vector<Location> liLocations;
 
    LoadVector(dennisAnchorLocations, dennisAnchorSrc);
+   LoadVector(dennisBodyLocations, dennisBodySrc);
+   LoadVector(dennisTitleLocations, dennisTitleSrc);
+   LoadVector(dennisUrlLocations, dennisUrlSrc);
    LoadVector(docEndLocations, docEndSrc);
+   LoadVector(rootIsrLocations, rootIsrSrc);
    //LoadVector(liLocations, liSrc);
 
-   Isr dennis(dennisAnchorLocations);
+   Isr dennisAnchor(dennisAnchorLocations);
+   Isr dennisBody(dennisBodyLocations);
+   Isr dennisTitle(dennisTitleLocations);
+   Isr dennisUrl(dennisUrlLocations);
+   Isr rootIsr(rootIsrLocations);
    //Isr li(liLocations);
    IsrEndDoc docEnd(docEndLocations);
 
-
-
-
+   Vector<Isr*> anchorTextIsrVec;
+   anchorTextIsrVec.push_back(&dennisAnchor);
    
+   Vector<Isr*> bodyTextIsrVec;
+   bodyTextIsrVec.push_back(&dennisBody);
 
+   Vector<Isr*> titleIsrVec;
+   titleIsrVec.push_back(&dennisTitle);
 
+   Vector<Isr*> urlIsrVec;
+   urlIsrVec.push_back(&dennisUrl);
+
+   DecoratedWordIsrs decWords = {anchorTextIsrVec, bodyTextIsrVec, titleIsrVec, urlIsrVec};
+
+   Ranker ranker;
+   Vector<ScoredDocument> topTen = ranker.Rank(&rootIsr, decWords, &docEnd);
+   
+   for(int i = 0; i < topTen.size(); ++i)
+      {
+      printf("%d", topTen[i].id);
+      printf("\n");
+      printf("%d", topTen[i].score); 
+      printf("\n");
+      }
    }
