8afee5a9ddd2e20606fea2d03275ab0705d8617c
Jacob Crouch <jwcrouch@umich.edu>
2019-04-23 00:50:32 -0400

    Doesn't compile. What it should look like.

--- include/expression.h
+++ include/expression.h
@@ -10,118 +10,202 @@
 #include <stdint.h>
 #include <vector>
 #include <cstddef>
-#include <string>
+#include "String.hpp"
+#include "index.hpp"
 using namespace std;
 
-/**
- * Just a plain old expression
- */
-class Expression
-{
-public:
-    
-    virtual ~Expression( );
-    
-    //virtual int64_t Eval( ) const = 0;
-    virtual string stringEval( ) const = 0;
-};
-// class Expression
-class Phrase: public Expression
-{
-protected:
-    
-    string value;
-    
-public:
-    
-    Phrase( string val );
-    
-    string stringEval( ) const override;
+typedef unsigned long long Location;
+typedef size_t FileOffset;
+
+//Not really sure what attributes are used for but they were in the lecture slides
+class WordAttributes{
+    String word;
+    bool isBold;
+    bool isItalicized;
+    bool isHighlighted;
+    bool isTitle;
 };
 
-class ANDExpression : public Expression
-{
-protected:
-    std::vector < Expression * > terms;
-public:
-    void addTerm(Expression *);
-    
-    string stringEval( ) const override
-    {
-        string phrase = terms[ 0 ]->stringEval( );
-        for ( size_t i = 1;  i < terms.size( );  ++i ) {
-            phrase += "&" + terms[ i ]->stringEval( );
-        }
-        return phrase;
-    }
+class DocumentAttributes{
+    int docID;
+    int docLength;
+    int numWords;
+    String url;
 };
 
-class AddExpression : public Expression
-{
-protected:
-    std::vector < Expression * > terms;
+class ISR{
 public:
-    void addTerm(Expression *);
-    
-    string stringEval( ) const override
-    {
-        string phrase = terms[ 0 ]->stringEval( );
-        for ( size_t i = 1;  i < terms.size( );  ++i ) {
-            phrase += " " + terms[ i ]->stringEval( );
-        }
-        return phrase;
+    Vector<ISR*> parseQuery(String &query);
+    ISR (String &query){
+        parseQuery(query);
     }
+    ISR (){}
+    
+    Vector<ISR*> queries;
+    //ISR pointer to the end of the current doc
+    ISR* docEnd;
+    
+    //Find next instance of a term
+    virtual Location nextInstance();
+    
+    //Finds the first occurrence of a term just pass the end of the document
+    virtual Location nextDocument();
+    
+    //Similar to nextDocument, finds the first occurrence of a term just at 'target' or later
+    virtual Location seek(Location target);
+    
+    //First number in the posting list of a term
+    virtual Location getClosestStartLocation();
+    
+    //Last number in the posting list of a term
+    virtual Location getClosestEndLocation();
+    
+    //Returns whatever document you're looking at
+    virtual ISR* getDocISR();
 };
 
-class OrExpression : public Expression
-{
-protected:
-    std::vector < Expression * > terms;
+class ISRWord : public ISR{
 public:
-    void addTerm(Expression *);
+    //Word to be looked up
+    String word;
     
-    string stringEval( ) const override
-    {
-        string phrase = terms[ 0 ]->stringEval( );
-        for ( size_t i = 1;  i < terms.size( );  ++i ) {
-            phrase += "|" + terms[ i ]->stringEval( );
-        }
-        return phrase;
+    //Basic constructor - does nothing
+    ISRWord(){};
+    
+    //Custom constructor
+    ISRWord(String wordToInsert){
+        word = wordToInsert;
     }
+    //Points to the current location on a document
+    Location getCurrentPost();
+    
+    //Gets number of documents the word appears in
+    unsigned getDocumentCount();
+    
+    //Gets number of times the word appears in index
+    unsigned getNumberOfOccurences();
+    
+    //Note: ISRWord will use its parent class's functions for nextInstance,
+    //nextDocument, seek, getPostsStart, getPostsEnd, getDocISR
 };
 
-class ParenthOrExpression : public Expression
-{
-protected:
-    std::vector < Expression * > terms;
+
+
+class ISRPhrase : public ISR{
 public:
-    void addTerm(Expression *);
+    void addTerm(ISR *);
     
-    string stringEval( ) const override
-    {
-        string phrase = terms[ 0 ]->stringEval( );
-        for ( size_t i = 1;  i < terms.size( );  ++i ) {
-            phrase += " " + terms[ i ]->stringEval( );
-        }
-        return phrase;
+    //Keeps track of how many terms we have
+    unsigned numOfTerms = 0;
+    
+    ISRPhrase(){}
+    //Constructor for ISRPhrase
+    ISRPhrase(String phraseToStore);
+    
+    Location seek(Location target);
+    //TODO
+    // 1. Seek all ISRs to the first occurrence beginning at the target location.
+    // 2. Pick the furthest term and attempt to seek all the other terms to the
+    //first location beginning where they should appear relative to the furthest term.
+    // 3. If any term is past the desired location, return to step 2.
+    // 4. If any ISR reaches the end, there is no match.
+    
+    //Finds next instance of phrase match
+    Location nextInstance(){
+        return seek(nearestStartLocation + 1);
     }
+    
+private:
+    unsigned nearestTerm, farthestTerm;
+    Location nearestStartLocation, nearestEndLocation;
+    //List of ISRs that we keep track of
+    Vector<ISR*> terms;
 };
 
-class SubExpression : public Expression
-{
-protected:
-    std::vector < Expression * > terms;
+
+
+class ISRAnd : public ISR{
 public:
-    void addTerm(Expression *);
+    void addTerm(ISR *);
     
-    string stringEval( ) const override
-    {
-        string phrase = "!" + terms[ 0 ]->stringEval( );
-        for ( size_t i = 1;  i < terms.size( );  ++i ) {
-            phrase += " " + terms[ i ]->stringEval( );
-        }
-        return phrase;
+    //Keeps track of how many terms we have
+    unsigned numOfTerms = 0;
+    
+    ISRAnd() {}
+    //Constructor for ISRAnd
+    ISRAnd(Vector<ISR> phrasesToInsert);
+    
+    Location seek(Location target);
+    //TODO:
+    // 1. Seek all the ISRs to the first occurrence beginning at
+    //    the target location.
+    // 2. Move the document end ISR to just past the furthest
+    //    word, then calculate the document begin location.
+    // 3. Seek all the other terms to past the document begin.
+    // 4. If any term is past the document end, return to step 2
+    // 5. If any ISR reaches the end, there is no match.
+    
+    //Finds next instance of all terms in a page
+    Location nextInstance(){
+        return seek(nearestStartLocation + 1);
     }
+    
+private:
+    unsigned nearestTerm, farthestTerm;
+    Location nearestStartLocation, nearestEndLocation;
+    //List of ISRs that we keep track of
+    Vector<ISR*> terms;
+};
+
+
+
+class ISROr : public ISR{
+public:
+    void addTerm(ISR *);
+    
+    
+    //Variable to keep track of how many terms are in 'terms' (because resize/reserve isn't implemented)
+    unsigned numOfTerms = 0;
+    
+    ISROr(){}
+    //Constructor for ISROr, MUST be in a vector<ISR> format, otherwise it wont compile
+    ISROr(Vector<ISR> phrasesToInsert);
+    
+    //Points to the closest 'beginning of page'
+    Location getClosestStartLocation(){
+        return nearestStartLocation;
+    }
+    
+    //Points to the closest 'end of page'
+    Location getClosestEndLocation(){
+        return nearestEndLocation;
+    }
+    
+    //Move all ISRs to the first occurrence of their respective word at 'target' or later
+    //Returns ULLONG_MAX if there is no match
+    Location seek(Location target);
+    //TODO:
+    // 1. Seek all the ISRs to the first occurrence beginning at
+    //    the target location.
+    // 2. Move the document end ISR to just past the furthest
+    //    word, then calculate the document begin location.
+    // 3. Seek all the other terms to past the document begin.
+    // 4. If any term is past the document end, return to step 2
+    // 5. If any ISR reaches the end, there is no match.
+    
+    //Find the next instance of ANY of the words in 'terms'
+    Location nextInstance();
+    
+    //Seek all ISRs to the first occurrence JUST PAST the end of the current document
+    Location nextDocument(){
+        return seek(docEnd->getPostsEndLocation() + 1);
+    }
+private:
+    //
+    //List of ISRs that we keep track of
+    Vector<ISR*> terms;
+    unsigned nearestTerm;
+    Location nearestStartLocation, nearestEndLocation;
 };
 
 #endif /* EXPRESSION_H_ */
