80bfafef163361852684639a9acad9dfaf9784e0
Jacob Crouch <jwcrouch@umich.edu>
2019-04-25 21:23:17 -0400

    Update rank_words.h to include adjs, verbs, more nouns, adverbs

--- include/rank_words.h
+++ include/rank_words.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "String.h"
 #include "vector.h"
 #include <fstream>
@@ -8,8 +9,13 @@ private:
    Vector<std::string> first_names;
    Vector<std::string> last_names;
    Vector<std::string> nouns;
+   Vector<std::string> adjectives;
+   Vector<std::string> verbs;
+   Vector<std::string> adverbs;
+
    Vector<std::pair<std::string, long>> popular_words;
    
+   //names, nouns, verbs, adjectives
    char type;
    long rank = -1;
    
@@ -40,14 +46,26 @@ private:
    }
    
    char getType(const String &word) {
-      if(is_noun(word)) {
-         return 'n';
-      }
-      else if(is_first(word)) {
+      if(is_first(word)) {
+         if(is_last(word)) {
+            return 'b';
+         }
          return 'f';
       }
       else if(is_last(word)) {
          return 'l';
+      }
+      else if(is_noun(word)) {
+         return 'n';
+      }
+      else if(is_verb(word)) {
+         return 'v';
+      }
+      else if(is_adjective(word)) {
+         return 'j';
+      }
+      else if(is_adverb(word)) {
+         return 'd';
       }
       else {
          return 'x';
@@ -64,6 +82,18 @@ private:
    
    bool is_last(const String &word) {
       return binSearch(last_names, 0, (int)last_names.size(), word);
+   }
+   
+   bool is_verb(const String &word) {
+      return binSearch(verbs, 0, (int)verbs.size(), word);
+   }
+   
+   bool is_adjective(const String &word) {
+      return binSearch(adjectives, 0, (int)adjectives.size(), word);
+   }
+   
+   bool is_adverb(const String &word) {
+      return binSearch(adverbs, 0, (int)adverbs.size(), word);
    }
    
    bool binSearch( Vector<std::string> v, int left, int right, String val) {
@@ -91,7 +121,7 @@ private:
 public:
    
    RankWords() {
-      std::fstream fs("Popular_nouns.txt"); //existence
+      std::fstream fs("nouns.txt"); //existence
       std::string line;
       while (getline(fs, line)) {
          // store each line in the vector
@@ -103,19 +133,40 @@ public:
       while (getline(fs2, line)) {
          first_names.push_back(line);
       }
-      fs.close();
+      fs2.close();
       
       std::fstream fs3("sorted_last_names.txt"); //existence
       while (getline(fs3, line)) {
          // store each line in the vector
          last_names.push_back(line);
       }
-      fs.close();
+      fs3.close();
       
-      std::fstream fs4("sorted_word_frequency.txt"); //rank, type
+      std::fstream fs4("adjectives.txt"); //existence
+      while (getline(fs4, line)) {
+         // store each line in the vector
+         adjectives.push_back(line);
+      }
+      fs4.close();
+      
+      std::fstream fs5("adverbs.txt"); //existence
+      while (getline(fs5, line)) {
+         // store each line in the vector
+         adverbs.push_back(line);
+      }
+      fs5.close();
+      
+      std::fstream fs6("verbs.txt"); //existence
+      while (getline(fs6, line)) {
+         // store each line in the vector
+         verbs.push_back(line);
+      }
+      fs6.close();
+      
+      std::fstream fs7("sorted_word_frequency.txt"); //rank, type
       std::string domain;
       long rank;
-      while (getline(fs4, line)) {
+      while (getline(fs7, line)) {
          // store each line in the vector
          std::size_t pos = line.find(",");
          domain = line.substr(0, pos);
@@ -123,7 +174,7 @@ public:
          //global vector of alexa and rank
          popular_words.push_back(std::pair<std::string, long>(domain,rank));
       }
-      fs.close();
+      fs7.close();
    }
    
    std::pair<char, long> getWordRankAndType(const String &word) {
